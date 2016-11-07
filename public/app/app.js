0eeacdaadcdc0d6214c4e1a1f3dc44b71f69c19e
sarahpappas <pappas.sarah@gmail.com>
2016-11-07 00:08:19 -0800

    folder structure

---
+++ public/app/app.js
@@ -0,0 +1,21 @@
+angular.module("VirtualPetApp", ["ui.router"])
+.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", function($stateProvider, $urlRouterProvider, $locationProvider) {
+	$urlRouterProvider.otherwise("/");
+
+	$stateProvider
+	.state("home", {
+		url: "/",
+		templateUrl: "app/views/home.html"
+	})
+	.state("signup", {
+		url: "/signup",
+		templateUrl: "app/views/signup.html"
+	})
+	.state("play", {
+		url: "/play/{activity}",
+		templateUrl: "app/views/play.html"
+	})
+}])
+.run(["$rootScope", "$state", function($rootScope, $state) {
+	$rootScope.state = $state;
+}]);
