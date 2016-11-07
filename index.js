0586adce083e8117406b30fbe1ef3508b17f0b51
sarahpappas <pappas.sarah@gmail.com>
2016-11-07 09:05:17 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = expres();
+var app = express();
 var mongoose = require("mongoose");
 
 // mongoose.connect();
 
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+app.get("/*", function(req, res) {
+	console.log("hello from the backend");
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+var server = app.listen(process.env.PORT || 3000);
+
+module.exports = server;
