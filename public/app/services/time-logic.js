3db2cddcaad481f35d52de9e1f93d078fc69a8fe
sarahpappas <pappas.sarah@gmail.com>
2016-11-08 12:07:06 -0800

    add seperate calc function

--- public/app/services/time-logic.js
+++ public/app/services/time-logic.js
@@ -47,32 +47,65 @@ angular.module("VirtualPetApp")
         return $http.put("/api/users");
     };
 
+    this.onClick = function(data){
+        if (data.activity == "feed") {
+          // hopefully jacob is sending data.stats
+          this.calcMood(data.activity);
+        }
+    }.bind(this);
+
+    this.calcMood = function(activity) {
+        var now = Date.now();
+        console.log(activity);
+        console.log(this.actionInfos[activity]);
+        var actionInfo = this.actionInfos[activity];
+        var msUntilMissed = actionInfo.msUntilMissed;
+
+       
+            if(now > this.timeLastExecuted + msUntilMissed) {
+                console.log("if ran");
+                //+= actionInfo.moodDeltas.missed 
+                if(this.mood + actionInfo.moodDeltas.missed < 0){
+                    this.mood = 0;
+                } else {
+                    this.mood += actionInfo.moodDeltas.missed;
+                }
+                if(this.health + actionInfo.healthDeltas.missed < 0){
+                    this.health = 0
+                } else {
+                    this.health += actionInfo.healthDeltas.missed;
+                }
+                $rootScope.$broadcast("update", this);
+            }
+    }.bind(this);
+
 
     this.checkForUpdate = function() {
             console.log("fire");
+            this.calcMood("feed");
 
-            var run = function() {
-                console.log("Run ran");
-                var now = Date.now();
+            // var run = function() {
+            //     console.log("Run ran");
+            //     var now = Date.now();
                 
-                var actionInfo = this.actionInfos.feed;
-                var msUntilMissed = actionInfo.msUntilMissed;
-                if(now > this.timeLastExecuted + msUntilMissed) {
-                    console.log("if ran");
-                    //+= actionInfo.moodDeltas.missed 
-                    if(this.mood + actionInfo.moodDeltas.missed < 0){
-                        this.mood = 0;
-                    } else {
-                        this.mood += actionInfo.moodDeltas.missed;
-                    }
-                    if(this.health + actionInfo.healthDeltas.missed < 0){
-                        this.health = 0
-                    } else {
-                        this.health += actionInfo.healthDeltas.missed;
-                    }
-                    $rootScope.$broadcast("update", this);
-                }
-            }.bind(this)();
+            //     var actionInfo = this.actionInfos.feed;
+            //     var msUntilMissed = actionInfo.msUntilMissed;
+            //     if(now > this.timeLastExecuted + msUntilMissed) {
+            //         console.log("if ran");
+            //         //+= actionInfo.moodDeltas.missed 
+            //         if(this.mood + actionInfo.moodDeltas.missed < 0){
+            //             this.mood = 0;
+            //         } else {
+            //             this.mood += actionInfo.moodDeltas.missed;
+            //         }
+            //         if(this.health + actionInfo.healthDeltas.missed < 0){
+            //             this.health = 0
+            //         } else {
+            //             this.health += actionInfo.healthDeltas.missed;
+            //         }
+            //         $rootScope.$broadcast("update", this);
+            //     }
+            // }.bind(this)();
 
 
         // this.getStats()
@@ -104,3 +137,48 @@ angular.module("VirtualPetApp")
     this.mood = 100;
     this.health = 100;
 }]);
+
+
+//     function StatsCtrl(ApplicationService) {
+//     var health = 100;
+//     var mood = 100;
+//     var currentDate = Date.now();
+   
+//     function totalMood(playVar, feedVar) {
+//       ApplicationService.getStats();
+//       if (mood <= 0) {
+//         health -= 25;
+//       }
+//       else if(currentDate > ApplicationService.pet.feed.next) {
+//         mood -= 25;
+//       }
+//       }
+//       else if(currentDate > ApplicationService.pet.exercise.next) {
+//         mood -= 25;
+//       }
+//       return $http({
+//         method: 'PUT',
+//         url: '/api/users/stats',
+//         data: {mood: mood}
+//       });    
+//     }
+//     function totalHealth(ApplicationService) {
+//       ApplicationService.getStats()
+//       if(currentDate > ApplicationService.pet.sleep.next) {
+//         health -= 25;
+//       }
+//       else if(currentDate > ApplicationService.pet.clean.next) {
+//         health -= 25;
+//       }
+//       else if(currentDate > ApplicationService.pet.feed.next) {
+//         health -= 25;
+//       }
+//       return $http({
+//         method: 'PUT',
+//         url: '/api/users/stats',
+//         data: {health: health}
+//       });    
+//     }
+//     }
+// StatsCtrl.$inject = ['ApplicationService'];
+// })()
