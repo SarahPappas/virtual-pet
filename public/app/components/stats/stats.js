0c8ae145de0dc4546d5fd28645d50078fc9f4145
jedimasterclos <carlossrinivas@gmail.com>
2016-11-08 14:19:14 -0800

    Carlos: who knows?

--- public/app/components/stats/stats.js
+++ public/app/components/stats/stats.js
@@ -6,33 +6,27 @@
 		controllerAs: "stats"
 	});
 
-	function StatsCtrl(TimeLogicService) {
+	function StatsCtrl(ApplicationService) {
     var health = 100;
     var mood = 100;
     var currentDate = Date.now();
-    var foodMaxUp = 0;
-    var foodMoodUp = 0;
-    var sleepMaxUp = 0;
-    var playMaxUp = 0;
-    var cleanMaxUp = 0;
+   
 
     function totalMood(playVar, feedVar) {
-      foodMoodUp = Math.floor((currentDate - TimeLogicService.pet.feed.last) / 4) * 25
-      playMaxUp= Math.floor((currentDate - TimeLogicService.pet.exercise.last) / 4) * 25
 
-      TimeLogicService.getStats();
+      ApplicationService.getStats();
 
       if (mood <= 0) {
         health -= 25;
       }
-      else if(currentDate > TimeLogicService.pet.feed.next) {
+      else if(currentDate > ApplicationService.pet.feed.next) {
         mood -= 25;
       }
       else if(foodMoodUp > 0){
         mood += 12.5;
         foodMoodUp -= 12.5;
       }
-      else if(currentDate > TimeLogicService.pet.exercise.next) {
+      else if(currentDate > ApplicationService.pet.exercise.next) {
         mood -= 25;
       }
       else if(playMaxUp > 0){
@@ -46,28 +40,28 @@
       });    
     }
 
-    function totalHealth(TimeLogicService) {
-      foodMaxUp = Math.floor((currentDate - TimeLogicService.pet.feed.last) / 4) * 25
-      sleepMaxUp = Math.floor((currentDate - TimeLogicService.pet.sleep.last) / 4) * 25
-      cleanMaxUp = Math.floor((currentDate - TimeLogicService.pet.clean.last) / 4) * 25
+    function totalHealth(ApplicationService) {
+      foodMaxUp = Math.floor((currentDate - ApplicationService.pet.feed.last) / 4) * 25
+      sleepMaxUp = Math.floor((currentDate - ApplicationService.pet.sleep.last) / 4) * 25
+      cleanMaxUp = Math.floor((currentDate - ApplicationService.pet.clean.last) / 4) * 25
 
-      TimeLogicService.getStats()
+      ApplicationService.getStats()
 
-      if(currentDate > TimeLogicService.pet.sleep.next) {
+      if(currentDate > ApplicationService.pet.sleep.next) {
         health -= 25;
       }
       else if(sleepMaxUp > 0) {
         health += 12.5;
         sleepMaxUp += 12.5;
       }
-      else if(currentDate > TimeLogicService.pet.clean.next) {
+      else if(currentDate > ApplicationService.pet.clean.next) {
         health -= 25;
       }
       else if(cleanMaxUp > 0) {
         health += 12.5;
         cleanMaxUp += 12.5;
       }
-      else if(currentDate > TimeLogicService.pet.feed.next) {
+      else if(currentDate > ApplicationService.pet.feed.next) {
         health -= 25;
       }
       else if(foodMaxUp > 0){
@@ -82,6 +76,6 @@
     }
 
 	}
-StatsCtrl.$inject = ['TimeLogicService'];
+StatsCtrl.$inject = ['ApplicationService'];
 })()
 
--- public/app/services/time-logic.js
+++ public/app/services/time-logic.js
@@ -1,108 +1,167 @@
 angular.module("VirtualPetApp")
-.service("TimeLogicService", ["$http", function($http) {
-	this.pet = [
-		sleep: {
-			last: "",
-			next: ""
-		},
-		feed: {
-			last: "",
-			next: ""
-		},
-		clean: {
-			last: "",
-			next: ""
-		},
-		exercise: {
-			last: "",
-			next: ""
-		},
-		nurse: {
-			last: "",
-			next: ""
-		],
-	};
+.service("ApplicationService", ["$http", "$rootScope", function($http, $rootScope) {
+  this.data = {
+    health: 100,
+    mood: 100,
+    stats: {
+      activity: "feed",
+      timeExecuted: Date.now()
+    }
+  }  
+  
 
-	var millisecondHr = 3600000;
+  this.msPerHour = 1000 * 60 * 60;
 
-	this.actionBy = {
-		sleep: {
-			hours: 10,
-			health: {
-				missed: .25,
-				acted: .125,
-				effect: 0
-			}, 
-		},
-		feed: {
-			hours: 4,
-			mood: {
-				missed: .25,
-				acted: .125,
-				effect: 0
-			},
-			health: {
-				missed: .25,
-				acted: .125,
-				effect: 0
-			}
-		},
-		clean: {
-			hours: millisecondHr/4,
-			health: {
-				missed: .25,
-				effect: 0
-			}
-		},
-		exercise: {
-			hours: 4
-			health: {
-				missed: .15,
-				acted: .75,
-				effect: 0
-			}
-		},
-		nurse: {
-			hours: 0;
-			health: {
-				acted: Math.random() * (1 - .5) + .5;
-			}
-		}
-	}
+  this.timeLastExecuted = Date.now();
+  // constants
+  this.actionInfos = {
+      sleep: {
+      },
+      feed: {
+          // msUntilNeeded: 4 * this.msPerHour,
+          msUntilMissed: 6000,
+          // msUntilMissed: 5 * this.msPerHour,
+          moodDeltas: {
+              missed: -20,
+              acted: 10,
+          },
+          healthDeltas: {
+              missed: -20,
+              acted: 10,
+          }
+      },
+      clean: {
+      },
+      exercise: {
+      },
+      nurse: {
+      }
+  }
 
-	// call onlogin, setTimeouts
-	this.getStats = function() {
-		return $http({
-			url: "/api/users",
-			method: "GET"
-		}).then(function(res) {
-			return res.data;
-		});
-	};
+  // call onlogin, setTimeouts
+  // gets all stats
+  this.getStats = function() {
+      return $http({
+          url: "/api/users",
+          method: "GET"
+      }).then(function(res) {
+          return res.data;
+      });
+  };
 
-	this.saveStats = function(activity, lastDate, nextDate) {
-		// finish put route for stats
-		return $http.put("/api/users");
-	};
+  this.saveStats = function(activity, lastDate, mood, health) {
+      // finish put route for stats
+      return $http({
+        url: "/api/users/stats",
+        method: "PUT",
+        data: {
+          activity: activity,
+          lastTime: lastDate,
+          mood: mood,
+          health: health
+        }
+      });
+  };
 
-	var feedTimeoutID;
-	var playTimeoutID;
-	var sleepTimeoutID;
-	var exerciseTimeoutID;
-	var cleanTimeoutID;
-	var gameoverTimeoutID;
+  this.onClick = function(currentActivity){
+    this.calcMood(currentActivity);
+  
+  }.bind(this);
 
-	// Interval or game loop
-	// function feedTimeout(timeToFeed) {
-	//   feedTimeoutID = window.setTimeout(function() {}
-	//   	, timeToFeed);
-	// }
+  this.calcMood = function(activity) {
 
-	function isGameover(){
-		//call health
-	}
+      var now = Date.now();
+      console.log(activity);
+      console.log(this.actionInfos[activity]);
+      var actionInfo = this.actionInfos[activity];
+      var msUntilMissed = actionInfo.msUntilMissed;
 
-	this.mood;
-	this.health;
+     
+          if(now > this.timeLastExecuted + msUntilMissed) {
+              console.log("if ran");
+              //+= actionInfo.moodDeltas.missed 
+              if(this.mood + actionInfo.moodDeltas.missed < 0){
+                  this.mood = 0;
+              } else {
+                  this.mood += actionInfo.moodDeltas.missed;
+              }
+              if(this.health + actionInfo.healthDeltas.missed < 0){
+                  this.health = 0
+              } else {
+                  this.health += actionInfo.healthDeltas.missed;
+              }
+              $rootScope.$broadcast("update", this);
+          }
+  }.bind(this);
 
+
+  this.checkForUpdate = function() {
+          console.log("fire");
+          this.calcMood("feed");
+
+      // this.getStats()
+      //     .then(function(stats) {
+      //         this.calMood(stats.activity);
+      //     }.bind(this));
+
+
+
+      // get stats from db
+      // do calculations
+      // if missed acition broadcast "miss" to stats component
+          //>> if currentTime-nextTime = 0
+          // then sets action's next time
+      // if stats are different broadcast to components for update
+  }.bind(this);
+
+  // game loop, where should this be called?
+  setInterval(this.checkForUpdate, 3000);
+
+  this.mood = 100;
+  this.health = 100;
 }]);
+
+
+//     function StatsCtrl(ApplicationService) {
+//     var health = 100;
+//     var mood = 100;
+//     var currentDate = Date.now();
+   
+//     function totalMood(playVar, feedVar) {
+//       ApplicationService.getStats();
+//       if (mood <= 0) {
+//         health -= 25;
+//       }
+//       else if(currentDate > ApplicationService.pet.feed.next) {
+//         mood -= 25;
+//       }
+//       }
+//       else if(currentDate > ApplicationService.pet.exercise.next) {
+//         mood -= 25;
+//       }
+//       return $http({
+//         method: 'PUT',
+//         url: '/api/users/stats',
+//         data: {mood: mood}
+//       });    
+//     }
+//     function totalHealth(ApplicationService) {
+//       ApplicationService.getStats()
+//       if(currentDate > ApplicationService.pet.sleep.next) {
+//         health -= 25;
+//       }
+//       else if(currentDate > ApplicationService.pet.clean.next) {
+//         health -= 25;
+//       }
+//       else if(currentDate > ApplicationService.pet.feed.next) {
+//         health -= 25;
+//       }
+//       return $http({
+//         method: 'PUT',
+//         url: '/api/users/stats',
+//         data: {health: health}
+//       });    
+//     }
+//     }
+// StatsCtrl.$inject = ['ApplicationService'];
+// })()
