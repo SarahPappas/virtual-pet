95089b38143e74895baf1aa70dfe331921caab0d
jedimasterclos <carlossrinivas@gmail.com>
2016-11-07 16:35:31 -0800

    Carlos: logic

--- public/app/components/stats/stats.js
+++ public/app/components/stats/stats.js
@@ -10,6 +10,7 @@
     var health = 100;
     var mood = 100;
     var currentDate = Date.now()
+    var rateOfChangeMood = 
 
     function totalMood(playVar, feedVar) {
       if (mood <= 0) {
@@ -17,10 +18,22 @@
       }
       else if(currentDate > TimeLogicService.pet.feed.next) {
         mood -= 25;
+        if (currentDate < TimeLogicService.pet.exercise.next)
       }
-      else if(currentDate > TimeLogicService.pet.sleep.next) {
-        
+      else if(currentDate > TimeLogicService.pet.exercise.next) {
+        mood -= 25;
       }
+
+    }
+
+    function totalHealth(TimeLogicService) {
+      if(currentDate > TimeLogicService.pet.sleep.next) {
+        health -= 25;
+      }
+      else if(currentDate > TimeLogicService.pet.clean.next) {
+        health -=25;
+      }
+      else if () {}
     }
 
 	}
