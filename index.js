f1295aee67601bda5cae504ec03305769f7fecd3
sarahpappas <pappas.sarah@gmail.com>
2016-11-11 14:25:19 -0800

    heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var mongoose = require("mongoose");
 
 var User = require('./models/user');
 // mongoose.connect();
-mongoose.connect('mongodb://localhost/nanopetusers');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/nanopetusers');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
