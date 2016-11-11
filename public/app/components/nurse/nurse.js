1275e77b6d17e34f109c9e246d7e8790c5beab1c
sarahpappas <pappas.sarah@gmail.com>
2016-11-11 13:02:58 -0800

    working on nurse availability logic

--- public/app/components/nurse/nurse.js
+++ public/app/components/nurse/nurse.js
@@ -6,8 +6,42 @@
 		controllerAs: "nurse"
 	});
 
-	function Nurse(ApplicationService) {
+	function Nurse(ApplicationService, $scope) {
+		var nurse = this;
+		nurse.data = ApplicationService;
+		nurse.isNurseAllowed = false;
+
+		$scope.safeApply = function(fn) {
+		  var phase = this.$root.$$phase;
+		  if(phase == '$apply' || phase == '$digest') {
+		    if(fn && (typeof(fn) === 'function')) {
+		      fn();
+		    }
+		  } else {
+		    this.$apply(fn);
+		  }
+		};
+
+		$scope.$on("update", function(event, args) {
+		    $scope.safeApply();
+			console.log("nurse allowed", nurse.isNurseAllowed);
+
+		})
+
+		console.log("nurse action infos", Number(ApplicationService.stats[4].last) + ApplicationService.actionInfos.nurse.msUntilAvailable);
+		if(Date.now() < Number(ApplicationService.stats[4].last) + ApplicationService.actionInfos.nurse.msUntilAvailable)
+		{
+			nurse.isNurseAllowed =  false;
+		} else {
+			nurse.isNurseAllowed =  true;
+		}
+
+
+		nurse.healing = function(){
+			ApplicationService.calcStats("nurse", "acted");
+		}
+
 		ApplicationService.startLoop();
 	}
-	Nurse.$inject = ["ApplicationService"];
+	Nurse.$inject = ["ApplicationService", "$scope"];
 })()
