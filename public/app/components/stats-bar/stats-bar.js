4aabe1bf1a23847759d3599cea021087d63284e0
Jacob-Elder <jacobelderonly@gmail.com>
2016-11-12 18:53:46 -0800

    stats bar is now populating as soon as the page loads

--- public/app/components/stats-bar/stats-bar.js
+++ public/app/components/stats-bar/stats-bar.js
@@ -11,10 +11,28 @@
 		console.log("StatsBarCtrl loaded!");
 
 		var StatsBarCtrl = this;
-		StatsBarCtrl.healthBars = [];
-		StatsBarCtrl.moodBars = [];
 
-		
+		$http({
+			url: "/api/users/stats",
+			method: "GET"
+		})
+		.then(function(res){
+			StatsBarCtrl.healthBars = [];
+            StatsBarCtrl.moodBars = [];
+            for(var i = 0; i < res.data.pet.health/10; i++){
+            	StatsBarCtrl.healthBars.push(i);
+            };
+            for(var j = 0; j < res.data.pet.mood/10; j++){
+            	StatsBarCtrl.moodBars.push(j);
+            }
+            if(res.data.pet.health >= 80){StatsBarCtrl.healthBarColor = "green"}
+            else if(res.data.pet.health >= 30){StatsBarCtrl.healthBarColor = "orange"}
+            else{StatsBarCtrl.healthBarColor = "red"};
+        	if(res.data.pet.mood >= 80){StatsBarCtrl.moodBarColor = "green"}
+            else if(res.data.pet.mood >= 30){StatsBarCtrl.moodBarColor = "orange"}
+            else{StatsBarCtrl.moodBarColor = "red"};
+
+		});
 
         // <-------- the only thing the feed needs to do is update server on click ------->
         StatsBarCtrl.data = ApplicationService;
@@ -34,7 +52,6 @@
             $scope.safeApply();
             StatsBarCtrl.healthBars = [];
             StatsBarCtrl.moodBars = [];
-            console.log("StatsBarCtrl health: " + StatsBarCtrl.data.health);
             for(var i = 0; i < StatsBarCtrl.data.health/10; i++){
             	StatsBarCtrl.healthBars.push(i);
             };
