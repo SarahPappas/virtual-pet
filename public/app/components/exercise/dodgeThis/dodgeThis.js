8ed672be2cffa4585ddb75a53fe5154e16b50649
jedimasterclos <carlossrinivas@gmail.com>
2016-11-12 23:54:16 -0800

    Carlos: Nearly finished with minigame

--- public/app/components/exercise/dodgeThis/dodgeThis.js
+++ public/app/components/exercise/dodgeThis/dodgeThis.js
@@ -14,6 +14,27 @@
     var touchright = false;
     var touchleft = true;
     var keysDown = {};
+    var time = Date.now();
+    var collision = setInterval(run, 10);
+
+    $scope.safeApply = function(fn) {
+      var phase = this.$root.$$phase;
+      if(phase == '$apply' || phase == '$digest') {
+        if(fn && (typeof(fn) === 'function')) {
+          fn();
+        }
+      } else {
+        this.$apply(fn);
+      }
+    };
+
+    $scope.$on("update", function(event, args) {
+      $scope.safeApply();
+    })
+
+    ApplicationService.getStats()
+      .then(function(res) {
+      });
 
     var mySprite =
      {
@@ -28,16 +49,16 @@
     var Sprites =
     {
        mySprite1 : {
-        x: Math.random() * 330,
+        x: Math.random() * 370,
         y: -50,
-        width: 40,
-        height: 40,
+        width: 30,
+        height: 30,
         speed: 120,
         color: '#0c0'
       },
 
       mySprite2:{
-        x: Math.random() * 340,
+        x: Math.random() * 390,
         y: -50,
         width: 40,
         height: 40,
@@ -53,96 +74,90 @@
         delete keysDown[e.keyCode];
     });
      
-    function update(mod)
-     {
-        if (37 in keysDown)
-      {
-            mySprite.x -= mySprite.speed * mod;
-        if(mySprite.x<0)
-        mySprite.x = 0;
-        if(mySprite.x == 0 && !touchleft)
-      {   
-        count+=10; 
-        var alert1 = document.getElementById("score");
-        alert1.innerHTML = "Score: " + count;
-        touchright=false;
-        touchleft=true;   
+    function update(mod) {
+      if (37 in keysDown) {
+        mySprite.x -= mySprite.speed * mod;
+          if(mySprite.x<0)
+            mySprite.x = 0;
+          if(mySprite.x == 0 && !touchleft) {   
+            count+=10; 
+            var alert1 = document.getElementById("dodgeScore");
+            alert1.innerHTML = "Score: " + count;
+            touchright=false;
+            touchleft=true;   
+          } 
       }
-        
+      if (39 in keysDown) {
+        mySprite.x += mySprite.speed * mod;
+          if(mySprite.x>400)
+            mySprite.x = 400;
+        if(mySprite.x == 400 && !touchright) {   
+          count+=10; 
+          var alert1 = document.getElementById("dodgeScore");
+          alert1.innerHTML = "Score: " + count;
+          touchright=true;
+          touchleft=false;    
         }
-        if (39 in keysDown)
-      {
-            mySprite.x += mySprite.speed * mod;
-        if(mySprite.x>360)
-        mySprite.x = 360;
-        
-        if(mySprite.x == 360 && !touchright)
-      {   
-        count+=10; 
-        var alert1 = document.getElementById("score");
-        alert1.innerHTML = "Score: " + count;
-        touchright=true;
-        touchleft=false;    
       }
-        }
+      else if (count >= 60) {
+        alert1.innerHTML= 'Congrats Mood Increased by 10%!'
+        clearInterval(collision);
+        ApplicationService.calcStats("exercise", "acted");  
+        setTimeout(function() {
+          $state.go('backPlay');
+        }, 3000);
+      } 
     }
 
-    function update1(mod)
-    {
-      for(index in Sprites)
-      {
-        if (Sprites.hasOwnProperty(index))
-        {
+
+
+    function update1(mod) {
+      for(index in Sprites) {
+        if (Sprites.hasOwnProperty(index)) {
           Sprites[index].y += Sprites[index].speed * mod;
         }   
       }
     }
 
-    var alert1 = document.getElementById("score");
-          alert1.innerHTML = "Score: " + count;
+    var alert1 = document.getElementById("dodgeScore");
+    alert1.innerHTML = "Score: " + count;
           
-    function render()
-    {
+    function render() {
       //canvas
       ctx.fillStyle = '#000';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-      
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
       //moving red object
-        ctx.fillStyle = mySprite.color;
-        ctx.fillRect(mySprite.x, mySprite.y, mySprite.width, mySprite.height);
+      ctx.fillStyle = mySprite.color;
+      ctx.fillRect(mySprite.x, mySprite.y, mySprite.width, mySprite.height);
+      //falling objects
+      for(index in Sprites) {
+        ctx.fillStyle = Sprites[index].color;
+        ctx.fillRect(Sprites[index].x, Sprites[index].y, Sprites[index].width, Sprites[index].height);
+          if (Sprites[index].y > 350) { 
+            Sprites[index].y = -50
+            Sprites[index].x = Math.random() * 400;    
+          }
+          
+          if (Sprites[index].x < mySprite.x + mySprite.width  && Sprites[index].x + Sprites[index].width  > mySprite.x &&
+          Sprites[index].y < mySprite.y + mySprite.height && Sprites[index].y + Sprites[index].height > mySprite.y) {       
+            alert1.innerHTML= "Game Over!"
+            clearInterval(collision);
+            setTimeout(function() {
+              $state.go('backPlay');
+            }, 3000);
 
-  //falling objects
-  for(index in Sprites)
-  {
-    ctx.fillStyle = Sprites[index].color;
-    ctx.fillRect(Sprites[index].x, Sprites[index].y, Sprites[index].width, Sprites[index].height);
-    if (Sprites[index].y > 300)
-    {  //Repeat the object when it falls out of view
-      Sprites[index].y = -50 //Account for the image size
-      Sprites[index].x = Math.random() * 400;    //Make it appear randomly along the width   
-    
+          }
+      }
     }
-    
-    if (Sprites[index].x < mySprite.x + mySprite.width  && Sprites[index].x + Sprites[index].width  > mySprite.x &&
-    Sprites[index].y < mySprite.y + mySprite.height && Sprites[index].y + Sprites[index].height > mySprite.y)
-    {       
-        alert("Game Over!");
-        clearInterval(collision);
-    }
-  }
-}
- 
-function run() 
-{
-    update((Date.now() - time) / 1000);
-  update1((Date.now() - time) / 1000);
-    render();
-    time = Date.now();
-}
+     
+    function run() {
+      update((Date.now() - time) / 1000);
+      update1((Date.now() - time) / 1000);
       
-var time = Date.now();
-var collision = setInterval(run, 10);
-  
+      render();
+      time = Date.now();
+    }
+           
     ApplicationService.startLoop(); 
   }
   DodgeCtrl.$inject = ["ApplicationService", '$state', '$scope'];
