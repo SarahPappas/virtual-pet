546287593cff43b0e284436ad8cadc4fbb138692
sarahpappas <pappas.sarah@gmail.com>
2016-11-13 12:13:41 -0800

    working on timeout and alert for feed

--- public/app/components/feed/feed.js
+++ public/app/components/feed/feed.js
@@ -9,27 +9,29 @@
     function Feed(ApplicationService, $scope, $timeout) {
         ApplicationService.startLoop();
         var feed = this;
+        feed.isFeedAllowed = true;
 
-        // <-------- the only thing the feed needs to do is update server on click ------->
-        // feed.data = ApplicationService;
+        $scope.safeApply = function(fn) {
+          var phase = this.$root.$$phase;
+          if(phase == '$apply' || phase == '$digest') {
+            if(fn && (typeof(fn) === 'function')) {
+              fn();
+            }
+          } else {
+            this.$apply(fn);
+          }
+        };
 
-        // $scope.safeApply = function(fn) {
-        //   var phase = this.$root.$$phase;
-        //   if(phase == '$apply' || phase == '$digest') {
-        //     if(fn && (typeof(fn) === 'function')) {
-        //       fn();
-        //     }
-        //   } else {
-        //     this.$apply(fn);
-        //   }
-        // };
+        $scope.$on("update", function(event, args) {
+            $scope.safeApply();
+            if(Date.now() > (Number(ApplicationService.stats[1].last) + (ApplicationService.actionInfos.feed.msUntilMissed / 2)))
+            {
+              feed.isNurseAllowed =  true;
+            } else {
+              feed.isNurseAllowed =  false;
+            }
+        })
 
-        // $scope.$on("update", function(event, args) {
-        //     $scope.safeApply();
-        // })
-
-
-        // <--------- remove above, update function below that runs on click -------->
 
         feed.feeding = function() {
             ApplicationService.calcStats("feed", "acted");
@@ -62,7 +64,7 @@
           }, 4000);
         }
 
-        
+
 
     }
 
